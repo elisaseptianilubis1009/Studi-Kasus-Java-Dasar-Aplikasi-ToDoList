54bc98a639dd2f6975e2e48408a94531fcbade0d
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-11-04 17:00:16 +0700

    Test Implementasi View Todo List

--- AplikasiToDoList/src/aplikasitodolist/AplikasiToDoList.java
+++ AplikasiToDoList/src/aplikasitodolist/AplikasiToDoList.java
@@ -6,12 +6,25 @@ public class AplikasiToDoList {
     public static String[] model= new String[10];
     
     public static void main(String[] args) {
-    
+        testShowTodoList();
     }
     
     //Menampilkan Todo List
     public static void showTodoList(){
+        for (int i=0;i<model.length;i++) {
+            String todo=model[i];
+            int no=i+1;
+            
+            if(todo != null){
+                System.out.println(no +". "+ todo);
+            }
+        }
+    }
     
+    public static void testShowTodoList(){
+        model[0]="Belajar Java Dasar";
+        model[1]="Membuat Studi Kasus Todo List";
+        showTodoList();
     }
     
     //Menambakan Todo ke list
