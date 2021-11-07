0597f4250fbc3ace017cebb391e526d393138f50
elisaseptiani1009 <elisaseptianlubis1009@gmail.com>
2021-11-07 12:11:05 +0700

    Test Implementasi Add Todo List

--- AplikasiToDoList/src/aplikasitodolist/AplikasiToDoList.java
+++ AplikasiToDoList/src/aplikasitodolist/AplikasiToDoList.java
@@ -6,7 +6,9 @@ public class AplikasiToDoList {
     public static String[] model= new String[10];
     
     public static void main(String[] args) {
+        testAddTodoList();
         testShowTodoList();
+          
     }
     
     //Menampilkan Todo List
@@ -22,14 +24,48 @@ public class AplikasiToDoList {
     }
     
     public static void testShowTodoList(){
-        model[0]="Belajar Java Dasar";
-        model[1]="Membuat Studi Kasus Todo List";
+//        model[0]="Belajar Java Dasar";
+//        model[1]="Membuat Studi Kasus Todo List";
         showTodoList();
     }
     
     //Menambakan Todo ke list
-    public static void addTodoList(){
+    public static void addTodoList( String todo){
+        
+        //cek index model apakah ada yang null
+        boolean isFull=true;
+        for(int i=0;i<model.length;i++){
+            if(model[i] == null){
+                isFull=false;
+                break;
+            }
+        }
+        
+        //Resize length model jika full
+        if(isFull){
+            String[] temp=model;
+            model= new String[model.length * 2];
+            
+            for(int i=0;i<temp.length;i++){
+                model[i]=temp[i];
+            }
+        }
+        
+        
+        //tambah todo ke model
+        for(int i =0;i<model.length;i++){
+            if(model[i] == null){
+                model[i]=todo;
+                break;
+            }
+        }
     
+    }
+    
+    public static void testAddTodoList(){
+        for(int i=0;i<25;i++){
+            addTodoList("Data To Do List ke - "+i);
+        }
     }
     
     //Mengahpus TodoList
